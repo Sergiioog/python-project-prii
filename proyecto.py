829722fc127070c86ea4a2b06afaf6d81a16a765
sergiioog <sergiogargor9@gmail.com>
2025-11-10 13:23:50 +0100

    feat(vector2D): vector2D Implementation

--- proyecto.py
+++ proyecto.py
@@ -190,4 +190,20 @@ class Vector2D:
       - __repr__(): 'Vector2D(x=?, y=?)'
       - length(self) -> float: norma Euclídea
     """
+
+    def __init__(self,x,y):
+        self.x = x
+        self.y = y
+
+    def __add__(self,other):
+        return Vector2D(self.x + other.x, self.y + other.y)
+
+    def __eq__(self,other):
+        return self.x == other.x and self.x == other.y
+
+    def __repr__(self):
+        return f"Vector2D(x={self.x}, y={self.y})"
+
+    def length(self) -> float:
+        return math.sqrt(self.x**2 + self.y**2)
     pass
---
+++ tests/04-test.py
@@ -0,0 +1,8 @@
+from proyecto import Vector2D
+
+vector1 = Vector2D(1,2)
+vector2 = Vector2D(3,4)
+print("Suma: ", vector1 + vector2)
+print("Equal: ", vector1 == vector2)
+print("Representation: ", vector1,vector2)
+print("Length: ", vector1.length(), vector2.length())
